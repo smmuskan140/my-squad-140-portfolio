23de061f530dc7c32c4b7e0b5744d80f8b9655f4
SMMuskan <mubashira.muskan.s.140@kalvium.community>
2026-03-09 14:36:59 +0530

    shit

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-﻿import Hero from '@/components/sections/Hero';
-import Navbar from '@/components/Navbar';
-import StatsSection from '@/components/sections/StatsSection';
-import MentorsSection from '@/components/sections/MentorsSection';
-import KalviansSection from '@/components/sections/KalviansSection';
-import EventsSection from '@/components/sections/EventsSection';
-import StudentsSection from '@/components/sections/StudentsSection';
-import GallerySection from '@/components/sections/GallerySection';
-import ContactSection from '@/components/sections/ContactSection';
-import Footer from '@/components/sections/Footer';
+import { DemoOne } from "@/components/ui/demo";
 
 export default function Home() {
-  return (
-    <main className="w-full">
-      <Navbar />
-      <Hero />
-      <StatsSection />
-      <MentorsSection />
-      <KalviansSection />
-      <EventsSection />
-      <StudentsSection />
-      <GallerySection />
-      <ContactSection />
-      <Footer />
-    </main>
-  );
+  return <DemoOne />;
 }
-
